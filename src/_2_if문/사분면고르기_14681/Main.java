60d76a9631f8ca479e18844469c830e1215b1c34
moonpeter <moonpeter@kakao.com>
2021-03-13 13:08:08 +0900

    2021.03.13

---
+++ src/_2_if문/사분면고르기_14681/Main.java
@@ -0,0 +1,22 @@
+package _2_if문.사분면고르기_14681;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else {
+            System.out.println("4");
+        }
+    }
+}
--- src/_2_if문/윤년_2753/Main.java
+++ src/_2_if문/윤년_2753/Main.java
@@ -1,7 +1,16 @@
 package _2_if문.윤년_2753;
 
-public class Main {
-    public static void main(String args[]) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a % 4 == 0 && (a % 100 !=0 || a % 400 ==0)) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
     }
 }
