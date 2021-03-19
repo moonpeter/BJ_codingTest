19092994bd083f30d2fa39a9b5b8fd0672f81f80
moonpeter <moonpeter@kakao.com>
2021-03-19 21:33:07 +0900

    2021.03.19

--- src/_3_for문/기찍N_2742/Main.java
+++ src/_3_for문/기찍N_2742/Main.java
@@ -2,6 +2,10 @@ package _3_for문.기찍N_2742;
 
 public class Main {
     public static void main(String[] args) {
-
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
