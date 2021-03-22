98c32e03375ae96a792303d121c2ba950688a4f7
moonpeter <moonpeter@kakao.com>
2021-03-22 17:12:49 +0900

    2021.03.22

--- src/_3_for문/별찍기1_2438/Main.java
+++ src/_3_for문/별찍기1_2438/Main.java
@@ -1,17 +1,22 @@
 package _3_for문.별찍기1_2438;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int N = 5;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-//        for(int i = 1; i<=5; i++) {
-//            System.out.println("*");
-//        }
+        String star[] = new String[N];
+        star[0] = "*";
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
+        for(int j=1; j<N; j++) {
+            star[j] = star[j-1] + "*";
+        }
+
+        for (int i=0; i<N; i++) {
+            System.out.println(star[i]);
+        }
     }
 }
